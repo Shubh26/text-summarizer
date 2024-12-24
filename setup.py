8ffe28fe9d47ad975c8a1b7f8dc3a2838b846d26
Shubham Gupta <shub.53@gmail.com>
2024-12-25 02:13:11 +0530

    requirement & installation done

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"
+AUTHOR_USER_NAME = "shubham"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "shub.53@gmail.com"
+
